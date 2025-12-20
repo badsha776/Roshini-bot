4dd638167ef87ffbda24a54b09fcf39cfce65a0b
Rishi <rishikeshanand51@gmail.com>
2025-12-20 20:24:55 +0530

    Update 0_sad_video.js

--- Priyansh/commands/0_sad_video.js
+++ Priyansh/commands/0_sad_video.js
@@ -19,7 +19,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["─━◉❖RAJ THAKUR ≛RAJ XD❖◉━─"];
+   var hi = ["─━◉❖Rishi❖◉━─"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
   "https://i.imgur.com/OHTcAGv.mp4",
